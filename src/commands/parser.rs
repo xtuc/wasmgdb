f8a709cc40609ca7bb8b408ab06579e57c88b720
Sven Sauleau <sven@cloudflare.com>
2022-11-29 12:26:49 +0000

    examine

--- src/commands/parser.rs
+++ src/commands/parser.rs
@@ -4,12 +4,16 @@ use nom::bytes::complete::take_while1;
 use nom::character::complete::alpha1;
 use nom::character::complete::digit1;
 use nom::character::complete::space1;
+use nom::character::complete::{char, one_of};
 use nom::character::is_alphabetic;
 use nom::combinator::map;
-use nom::combinator::map_res;
 use nom::combinator::opt;
+use nom::combinator::recognize;
+use nom::multi::{many0, many1};
 use nom::sequence::delimited;
+use nom::sequence::pair;
 use nom::sequence::preceded;
+use nom::sequence::terminated;
 use nom::sequence::tuple;
 use nom::IResult;
 
@@ -19,7 +23,7 @@ fn ident<'a>(input: &'a str) -> IResult<&'a str, &'a str> {
     take_while1(|c: char| is_alphabetic(c as u8) || c == '_')(input)
 }
 
-fn parse_single_expr<'a>(input: &'a str) -> IResult<&'a str, Expr<'a>> {
+fn parse_name_expr<'a>(input: &'a str) -> IResult<&'a str, Expr<'a>> {
     map(ident, |n| Expr::Name(n))(input)
 }
 
@@ -27,12 +31,27 @@ fn parse_parens_expr<'a>(input: &'a str) -> IResult<&'a str, Expr<'a>> {
     delimited(tag("("), parse_expr, tag(")"))(input)
 }
 
+fn parse_hex_expr<'a>(input: &'a str) -> IResult<&'a str, Expr<'a>> {
+    let hex = preceded(
+        alt((tag("0x"), tag("0X"))),
+        recognize(many1(terminated(
+            one_of("0123456789abcdefABCDEF"),
+            many0(char('_')),
+        ))),
+    );
+    map(hex, |v: &'a str| {
+        let v = u32::from_str_radix(&str::replace(&v, "_", ""), 16).unwrap();
+        Expr::Hex(v)
+    })(input)
+}
+
 fn parse_expr<'a>(input: &'a str) -> IResult<&'a str, Expr<'a>> {
     alt((
         parse_expr_member_access,
         parse_parens_expr,
         parse_expr_deref,
-        parse_single_expr,
+        parse_hex_expr,
+        parse_name_expr,
     ))(input)
 }
 
@@ -44,7 +63,7 @@ fn parse_expr_deref<'a>(input: &'a str) -> IResult<&'a str, Expr<'a>> {
 
 /// Member access lhs accepts a single expression or a parenthesed expression
 fn parse_expr_member_access<'a>(input: &'a str) -> IResult<&'a str, Expr<'a>> {
-    let object = alt((parse_parens_expr, parse_single_expr));
+    let object = alt((parse_parens_expr, parse_name_expr));
 
     map(
         tuple((object, tag("->"), ident)),
@@ -57,6 +76,24 @@ pub(crate) fn parse_command<'a>(input: &'a str) -> IResult<&'a str, Command<'a>>
 
     Ok(match word {
         "bt" => (input, Command::Backtrace),
+        "x" => {
+            let (input, params) = opt(preceded(tag("/"), pair(digit1, opt(alpha1))))(input)?;
+            let params = if let Some((number, format)) = params {
+                let number = number.parse::<u32>().unwrap();
+                let format = match format {
+                    Some("s") => Some(PrintFormat::String),
+                    Some(_) => unimplemented!(),
+                    _ => None,
+                };
+
+                (Some(number), format)
+            } else {
+                (None, None)
+            };
+
+            let (input, what) = preceded(space1, parse_expr)(input)?;
+            (input, Command::Examine(what, params))
+        }
         "p" => {
             let (input, format) = opt(tag("/s"))(input)?;
 
